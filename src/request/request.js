c802732f4a0509813ce2fdb15ce9e526f4ba72aa
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2017-04-09 19:09:32 -0400

    Temporary RN workaround

--- src/request/request.js
+++ src/request/request.js
@@ -1,6 +1,1 @@
-var isNode = typeof navigator === 'undefined';
-
-// FIXME: how to do conditional loading this with ES6 modules?
-module.exports = isNode ?
-    require('./request.node') :
-    require('./request.polyfill');
+module.exports = require('./request.polyfill');
